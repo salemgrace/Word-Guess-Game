2b5eacc47be143df1ea8fab588e8ed82063e2816
salemgrace <salem.bryant@gmail.com>
2018-08-18 19:39:16 -0400

    new logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
     // Initial Variables
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+    // var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 
     var wordBank = ["rock", "paper", "scissors"];
 
@@ -7,35 +7,41 @@
     var wins = 0;
     var losses = 0;
 
+    var number = Math.floor(Math.random() * wordBank.length);
+
+
+    var number = Math.floor(Math.random() * wordBank.length);
+
+    var currentWord = wordBank[number];
     
+    var wordLength = currentWord.length;
+    console.log(wordLength);
+
+    var wordDashes = wordLength;
+
+    function resetGame () {
+
     document.onkeyup = function(event) {
         var userGuess = event.key;
-
-        var number = Math.floor(Math.random() * wordBank.length);
-
-        var currentWord = wordBank[number];
-            console.log(currentWord);
-
-        if (userGuess === letters) {
-            var html =
+        console.log(userGuess);
+        
+        var html =
             "<p>Letters guessed so far: " + userGuess + "</p>" +
             "<p>Gueses remaining: " + maxGuess + "</p>" +
             "<p>Wins: " + wins + "</p>" +
             "<p>Losses: " + losses + "</p>";
   
-          // Set the inner HTML contents of the #game div to our html string
-          document.querySelector("#stats").innerHTML = html;
-        }
+        document.querySelector("#stats").innerHTML = html;
 
+        // document.querySelector("#letters").innerHTML = wordDashes;
     };
 
-//     function resetGame () {
 
 //     //for-loop to iterate through the letters array
 //     for (var i = 0; i < letters.length; i++) {
 
 //         currentWord.push("_");
 
-//     };
 
-// };
+};
+    resetGame();
