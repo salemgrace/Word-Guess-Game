6cbd6e717dba064635b7abfeace1a84ab1e4a6db
salemgrace <salem.bryant@gmail.com>
2018-08-18 09:02:24 -0400

    new logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,39 @@
 
     var wordBank = ["rock", "paper", "scissors"];
 
-    var number = Math.floor(Math.random() * wordBank.length);
-
-    var currentWord = wordBank[number];
-        console.log(currentWord);
-
     var maxGuess = 10;
-
     var wins = 0;
-
     var losses = 0;
 
     
     document.onkeyup = function(event) {
         var userGuess = event.key;
 
+        var number = Math.floor(Math.random() * wordBank.length);
+
+        var currentWord = wordBank[number];
+            console.log(currentWord);
+
         if (userGuess === letters) {
-
-            
-
+            var html =
+            "<p>Letters guessed so far: " + userGuess + "</p>" +
+            "<p>Gueses remaining: " + maxGuess + "</p>" +
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Losses: " + losses + "</p>";
+  
+          // Set the inner HTML contents of the #game div to our html string
+          document.querySelector("#stats").innerHTML = html;
         }
 
     };
 
-    function resetGame () {
+//     function resetGame () {
 
-    //for-loop to iterate through the letters array
-    for (var i = 0; i < letters.length; i++) {
+//     //for-loop to iterate through the letters array
+//     for (var i = 0; i < letters.length; i++) {
 
-        currentWord.push("_");
+//         currentWord.push("_");
 
-    };
+//     };
 
-};
+// };
