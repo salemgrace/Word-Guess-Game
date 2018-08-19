24d21b7d968344643b26cac65baa3596462d8b87
salemgrace <salem.bryant@gmail.com>
2018-08-18 22:06:08 -0400

    html docs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,30 @@
     // Initial Variables
-    // var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-
     var wordBank = ["rock", "paper", "scissors"];
 
     var maxGuess = 10;
+    var guessArray = [];
+
     var wins = 0;
     var losses = 0;
+
+    var dashes;
 
     var number = Math.floor(Math.random() * wordBank.length);
 
     var currentWord = wordBank[number];
     console.log(currentWord);
     
-    var wordLength = currentWord.length;
-    document.getElementById("#letters").innerHTML = "_";
 
     function resetGame () {
 
+        for ( var i = 0; i < currentWord.length; i++)
+
+        {
+            guessArray[i] = "_";
+        }
+
+        dashes = guessArray.join(" ");
+        document.getElementById("letters").innerHTML = dashes;
 
 
 };
@@ -25,13 +33,15 @@
         var userGuess = event.key;
         console.log(userGuess);
         
-        var html =
-            "<p>Letters guessed so far: " + userGuess + "</p>" +
-            "<p>Gueses remaining: " + maxGuess + "</p>" +
-            "<p>Wins: " + wins + "</p>" +
-            "<p>Losses: " + losses + "</p>";
+        document.getElementById("letters-guessed").innerHTML = guessArray;
+        
+        // var html =
+        //     "<p>Letters guessed so far: " + userGuess + "</p>" +
+        //     "<p>Gueses remaining: " + maxGuess + "</p>" +
+        //     "<p>Wins: " + wins + "</p>" +
+        //     "<p>Losses: " + losses + "</p>";
   
-        document.querySelector("#stats").innerHTML = html;
+        // document.querySelector("#stats").innerHTML = html;
 
 };
     resetGame();
