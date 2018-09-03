e70ee1174e2f2f250263687905819fe6318a63d2
salemgrace <salem.bryant@gmail.com>
2018-09-02 23:17:53 -0400

    game updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,16 @@
     var wordBank = ["mezzanine", "proscenium", "director", "actor", "dramaturg", "ensemble", "crew", 
     "blocking", "tech", "aside", "backdrop", "flyrail", "masking", "scrim"];
 
-    var maxGuess = 10;
-
-    var guessArray = [];
-    var wrongGuessesArray = [];
+    var currentWord = "";
+    var lettersInCurrentWord = [];
+    var currentLettersAndBlanks = [];
 
     var dashes = 0;
 
-    var currentWord = "";
-    var lettersInCurrentWord = "";
-
+    var wrongGuessesArray = [];
+    var lettersGuessed = "";
+    
+    var maxGuess = 10;
     var wins = 0;
     var losses = 0;
     
@@ -33,18 +33,28 @@
     }
 
     function beginGame () {
-        document.getElementById("game-info").textContent = "What word am I thinking of?";
-        gameStarted = true;
 
+        maxGuess = 10;
+        
         currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-        dashes = currentWord.length;
 
         lettersInCurrentWord = currentWord.split(" ");
         
+        dashes = currentWord.length;
+        
+        console.log(currentWord);
+
+        currentLettersAndBlanks = [];
+
+        wrongGuessesArray = [];
+
         for ( var i = 0; i < currentWord.length; i++) {
             lettersInCurrentWord[i] = "_";
         }
 
+        document.getElementById("game-info").textContent = "What word am I thinking of?";
+        gameStarted = true;
+        
         dashes = lettersInCurrentWord.join(" ");
         document.getElementById("word-to-guess").innerHTML = dashes;
         document.getElementById("word-to-guess").innerHTML = lettersInCurrentWord.join(" ");
@@ -52,5 +62,35 @@
         guessArray.push(event.key);
         document.getElementById("letters-guessed").innerHTML = guessArray.join(" ");
 
+    }
 
-    };
+    function checkLetters(letter) {
+        var letterinWord = false;
+
+        for (var i = 0; i < dashes; i++) {
+            if (wordbank[i] === letter) {
+                letterInWord = true;
+            }
+        }
+
+        if (letterinWord) {
+
+            for (var j = 0; j < dashes; j++) {
+                if (wordBank[j] === letter) {
+                currentLettersAndBlanks[j] = letter;
+                }
+            }
+        }
+
+    }
+
+
+beginGame();
+
+document.onkeyup = function(event) {
+    lettersGuessed = String.fromCharCode(event.which).toLocaleLowerCase();
+
+    checkLetters(lettersGuessed);
+
+    roundComplete();
+};
